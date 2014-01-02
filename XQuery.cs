7ed59813dbd9da5165fdc48192dd7e5a8f49dacc
Alexej Haak <alexej.haak@outlook.de>
2014-01-02 02:51:25 +0100

    Initial Commit

---
+++ XQuery.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Android.Widget;
+using unirest_net.http;
+
+namespace XQuery
+{
+    public class XQuery
+    {
+        private View View { get; set; }
+        private Activity Activity { get; set; }
+        private View CurrentView { get; set; }
+        private readonly Boolean _useActivity;
+        private readonly Dictionary<int, Bitmap> _imageCache = new Dictionary<int, Bitmap>();
+
+        public XQuery(View view)
+        {
+            _useActivity = false;
+            View = view;
+        }
+
+        public XQuery(Activity activity)
+        {
+            _useActivity = true;
+            Activity = activity;
+        }
+
+        public XQuery Id(int id)
+        {
+            CurrentView = GetView(id);
+            return this;
+        }
+
+        public View GetView(int id)
+        {
+            return _useActivity ? Activity.FindViewById(id) : View.FindViewById(id);
+        }
+
+        public T GetView<T>(int id) where T : View
+        {
+            return _useActivity ? Activity.FindViewById<T>(id) : View.FindViewById<T>(id);
+        }
+
+        public XQuery Text(String text)
+        {
+            ((TextView)CurrentView).Text = text;
+            return this;
+        }
+
+        public XQuery Image(Drawable drawable)
+        {
+            ((ImageView)CurrentView).SetImageDrawable(drawable);
+            return this;
+        }
+
+        public XQuery Image(int resId)
+        {
+            ((ImageView)CurrentView).SetImageResource(resId);
+            return this;
+        }
+
+        public XQuery Image(Bitmap bitmap)
+        {
+            ((ImageView)CurrentView).SetImageBitmap(bitmap);
+            return this;
+        }
+
+        public async Task<XQuery> Image(String url)
+        {
+            var response = await Unirest.get(url).asBinaryAsync();
+            var bitmap = await BitmapFactory.DecodeStreamAsync(response.Body);
+            ((ImageView)CurrentView).SetImageBitmap(bitmap);
+            return this;
+        }
+
+        public async Task<XQuery> Image(String url, Boolean memCache)
+        {
+            var hash = url.GetHashCode();
+
+            if (_imageCache.ContainsKey(hash))
+                return Image(_imageCache[hash]);
+
+            var response = await Unirest.get(url).asBinaryAsync();
+            var bitmap = await BitmapFactory.DecodeStreamAsync(response.Body);
+            _imageCache.Add(hash, bitmap);
+            return Image(bitmap);
+        }
+    }
+}
